496d7733e9be696a3cd223c8a6b501b68c863155
Mariia Shytse <aleatoricmbnt@gmail.com>
2026-07-21 19:35:38 +0300

    added res/ds number to the local_exec

--- main/00_local_exec/main.tf
+++ main/00_local_exec/main.tf
@@ -9,12 +9,14 @@ variable "program_for_data_external" {
 }
 
 resource "terraform_data" "test" {
+  count = var.data_count
   provisioner "local-exec" {
     command = var.command_for_local_exec
   }
 }
 
 resource "terraform_data" "always_recreated" {
+  count = var.res_count
   triggers_replace = timestamp()
 }
 
@@ -22,4 +24,16 @@ variable "command_for_local_exec" {
   type = string
   description = "Command for local exec (single line; no newlines inside -c string)"
   default = "echo '{\"result\": \"test\"}'"
+}
+
+variable "data_count" {
+  type = number
+  description = "Number of data sources to create"
+  default = 1
+}
+
+variable "res_count" {
+  type = number
+  description = "Number of resources to create"
+  default = 1
 }
