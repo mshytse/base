77f355d08ed11fdf2184ca2cca9aa7c033b3190f
Mariia Shytse <aleatoricmbnt@gmail.com>
2026-07-29 16:31:10 +0300

    module with ref // update

--- config/modules/module_sources/module_github_com/main.tf
+++ config/modules/module_sources/module_github_com/main.tf
@@ -1,3 +1,22 @@
 module "smth_from_git" {
   source = "github.com/mshytse/temp?ref=masha-smash"
 }
+
+module "smth_from_git2" {
+  source = "github.com/mshytse/temp?ref=0.0.1"
+}
+module "smth_from_git3" {
+  source = "github.com/mshytse/temp?ref=1.0.0"
+}
+
+module "smth_from_git4" {
+  source = "github.com/mshytse/temp?ref=1.1.2"
+}
+
+module "smth_from_git5" {
+  source = "github.com/mshytse/temp?ref=1.11.2"
+}
+
+module "smth_from_git6" {
+  source = "github.com/mshytse/temp?ref=qqq"
+}
